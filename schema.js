0edfefd568e68f0ca6295e62a0342fd0f1c4b749
connie <conniedixon106@gmail.com>
2021-02-09 11:13:59 +0000

    ser up graphqllist and axios calls, exported schema

--- schema.js
+++ schema.js
@@ -1,6 +1,7 @@
 //This is the file for all graphql stuff
 
-const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean} = require("graphql")
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList, GraphQLSchema} = require("graphql")
+const axios = require("axios")
 
 //Launch type
 const LaunchType = new GraphQLObjectType({
@@ -26,4 +27,28 @@ const RocketType = new GraphQLObjectType({
         rocket_name: {type: GraphQLString},
         rocket_type: {type: GraphQLString},
     })
+})
+
+//This is the actual set up for making calls
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    // note object not function
+    fields: {
+        launches: {
+            //get an array of all launches
+            type: new GraphQLList(LaunchType),
+            resolve(parent, args) {
+                //make request to actual db inside here
+                return axios.get('https://api.spacexdata.com/v3/launches')
+                .then(res=>res.data)
+            }
+        }
+    }
+})
+
+//export as a GraphQL Schema
+module.exports = new GraphQLSchema({
+    query: RootQuery
+    //also takes in mutations here
 })
